c3e14d6f6de80b6c965a2769a5730bed5eb98b8a
MGup <MGup@MGUP-L-OFS.DIR.slb.com>
2012-02-02 17:11:02 -0600

    The app now recieves the last message timestamp from the master computer, decrypts it, and does some basic checks of it against the current and last sync time. It is now ready to start the code for actually retrieving the messages and sending them out over the link.

--- src/com/mason/smssync/receiver/SMSSyncService.java
+++ src/com/mason/smssync/receiver/SMSSyncService.java
@@ -1,6 +1,7 @@
 package com.mason.smssync.receiver;
 
 //import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.InetSocketAddress;
@@ -8,6 +9,7 @@ import java.net.Socket;
 import java.security.MessageDigest;
 
 import javax.crypto.Cipher;
+import javax.crypto.CipherInputStream;
 import javax.crypto.CipherOutputStream;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
@@ -37,19 +39,22 @@ public class SMSSyncService extends IntentService
 	{
 		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
 		String ipAddress = prefs.getString(SMSSyncAppConfigActivity.pSyncIPAddr, "");
-		byte[] password = prefs.getString(SMSSyncAppConfigActivity.pSyncPassword, "").getBytes(); //.toCharArray();
+		String lastSyncTimeStr = prefs.getString(SMSSyncAppConfigActivity.pLastSyncTime, "null");
+		byte[] password = prefs.getString(SMSSyncAppConfigActivity.pSyncPassword, "").getBytes();
 		byte[] salt = { (byte)0x3b, (byte)0x58, (byte)0x3a, (byte)0x8c, (byte)0x49, (byte)0xd3, (byte)0x21, (byte)0x88 };
-		byte[] marker = { (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00 };
 		byte[] finalPass = new byte[password.length + salt.length];
 		System.arraycopy(password, 0, finalPass, 0, password.length);
 		System.arraycopy(salt, 0, finalPass, password.length, salt.length);
 		
 		try
 		{
+			//generate the key for the encryption
 			MessageDigest md5er = MessageDigest.getInstance("MD5");
 			md5er.update(finalPass);
 			byte[] keyBytes = md5er.digest();
 			SecretKeySpec syncKey = new SecretKeySpec(keyBytes, "AES");
+			
+			//create the cipher objects and get the IV
 			Cipher sendCipher = Cipher.getInstance("AES/CFB8/NoPadding");
 			Cipher recCipher = Cipher.getInstance("AES/CFB8/NoPadding");
 			sendCipher.init(Cipher.ENCRYPT_MODE, syncKey);
@@ -57,20 +62,45 @@ public class SMSSyncService extends IntentService
 			recCipher.init(Cipher.DECRYPT_MODE, syncKey, decryptIv);
 			byte[] sendiv = sendCipher.getIV();
 			
+			//open the comm socket and transmit the IV
 			InetSocketAddress serverAddrPort = new InetSocketAddress(ipAddress, 1234);
 			Socket syncSocket = new Socket();
 			syncSocket.connect(serverAddrPort, 500);
 			OutputStream syncOut = syncSocket.getOutputStream();
 			syncOut.write(sendiv);
-			syncOut.write(marker);
+			//syncOut.write(marker);
 			syncOut.flush();
 			
+			//create the output stream and transmit the timestamp
+			//note that the remote computer will not acknowledge anything until a proper timestamp is
+			//decoded and verified, which proves that it is talking to this app with the correct password
 			CipherOutputStream syncOutStr = new CipherOutputStream(syncOut, sendCipher);
 			OutputStreamWriter out = new OutputStreamWriter(syncOutStr);
 			Time currentTime = new Time();
 			currentTime.setToNow();
 			String timeString = currentTime.format3339(false);
 			out.write(timeString);
+			out.flush();
+			
+			//set up the input stream, then receive and decode the last message timestamp and range-check it
+			CipherInputStream syncInStr = new CipherInputStream(syncSocket.getInputStream(), recCipher);
+			InputStreamReader in = new InputStreamReader(syncInStr);
+			char[] readData = new char[150];
+			int readLength = in.read(readData, 0, 150);
+			String receivedTimestamp = String.valueOf(readData);
+			Time rxTimestampTime = new Time();
+			rxTimestampTime.parse3339(receivedTimestamp);
+			if (rxTimestampTime.after(currentTime))
+				throw new Exception("Last message time is after present time!");
+			if (lastSyncTimeStr != "null")
+			{
+				Time lastSyncTime = new Time();
+				lastSyncTime.parse(lastSyncTimeStr);
+				if (rxTimestampTime.before(lastSyncTime))
+					throw new Exception("Last message time is before last sync time!");
+			}
+			
+			//close out the connections
 			out.close();
 			syncOutStr.close();
 			syncSocket.close();
